4b3f080c11e6992280a9380d087d313fa1982740
Kiryl Kakareka <charleequa@gmail.com>
2021-08-20 16:46:06 +0300

    Signing Requests

--- Flickr/Flickr/Services/AuthService.swift
+++ Flickr/Flickr/Services/AuthService.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class AuthService {
+    static let shared = AuthService()
+    
+    struct Constants {
+        static let key = "1877653cabad94b4cd42e56f49689e6c"
+        static let secretKey = "f1938a9c14a1d472"
+    }
+    
+    public var signWithURL: URL? {
+        let base = "https://www.flickr.com/services/oauth/request_token"
+        let requestURL = "https://www.github.com/CharleeQ"
+        let string = "\(base)?oauth_nonce=89601180&oauth_timestamp=1305583298&oauth_consumer_key=\(Constants.key)&oauth_signature_method=HMAC-SHA1&oauth_version=2.0&oauth_callback=\(requestURL)"
+        
+        return URL(string: string)
+    }
+}
--- Flickr/Flickr/ViewController.swift
+++ Flickr/Flickr/ViewController.swift
@@ -11,9 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Sign In"
+        view.backgroundColor = .systemPink
     }
-
 
 }
 
