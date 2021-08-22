6775c5a5ea4dca16aeeab3d6e1a112822eb59b29
Kiryl Kakareka <charleequa@gmail.com>
2021-08-22 20:27:34 +0300

    Getting request token

--- Flickr/Flickr/Services/AuthService.swift
+++ Flickr/Flickr/Services/AuthService.swift
@@ -6,20 +6,26 @@
 //
 
 import Foundation
+import Alamofire
 
 class AuthService {
     static let shared = AuthService()
     
-    struct Constants {
-        static let consumerKey = "1877653cabad94b4cd42e56f49689e6c"
-        static let consumerSecret = "f1938a9c14a1d472"
+    // MARK: - Parameters
+    
+    let consumerKey = "1877653cabad94b4cd42e56f49689e6c"
+    let consumerSecret = "f1938a9c14a1d472"
+    let callback = "www.github.com"
+    let nonce = "956r13465"
+    let timestamp = "1305586162"
+    let version = "1.0"
+    let signatureMethod = "HMAC-SHA1"
+    var signature: String {
+        let string = "GET&https%3A%2F%2Fwww.flickr.com%2Fservices%2Foauth%2Frequest_token&oauth_callback%3Dhttp%253A%252F%252F\(callback)%26oauth_consumer_key%3D\(consumerKey)%26oauth_nonce%3D\(nonce)%26oauth_signature_method%3D\(signatureMethod)%26oauth_timestamp%3D\(timestamp)%26oauth_version%3D\(version)"
+        return string.hmac(key: "\(consumerSecret)&")
     }
     
-    var signature: String {
-        let string = "GET&https%3A%2F%2Fwww.flickr.com%2Fservices%2Foauth%2Frequest_token&oauth_callback%3Dhttp%253A%252F%252Fwww.github.com%26oauth_consumer_key%3D1877653cabad94b4cd42e56f49689e6c%26oauth_nonce%3D956r13465%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1305586162%26oauth_version%3D1.0"
-        
-        return string.hmac(key: "f1938a9c14a1d472&")
-    }
+    var requestToken: String?
 }
 
 //"https://www.flickr.com/services/oauth/request_token?oauth_callback=http://www.github.com&oauth_consumer_key=1877653cabad94b4cd42e56f49689e6c&oauth_nonce=956r13465&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1305586162&oauth_version=1.0&oauth_signature=kDgmLbI4KS8KYmNv8OD1cR0ln74="
--- Flickr/Flickr/ViewController.swift
+++ Flickr/Flickr/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Sign In"
-        view.backgroundColor = .systemPink
+        
+        print("[1] Signing Requests")
+        print(AuthService.shared.signature)
+        
+        print("[2] Request Token")
+        AF.request("https://www.flickr.com/services/oauth/request_token?oauth_callback=http://\(AuthService.shared.callback)&oauth_consumer_key=\(AuthService.shared.consumerKey)&oauth_nonce=\(AuthService.shared.nonce)&oauth_signature_method=\(AuthService.shared.signatureMethod)&oauth_timestamp=\(AuthService.shared.timestamp)&oauth_version=\(AuthService.shared.version)&oauth_signature=\(AuthService.shared.signature)").responseString { response in
+            switch response.result {
+            case .success(let data):
+                print(data)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
-
+    
 }
 
