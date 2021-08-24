2f1d3d35790d40546a96440ccb2196717e651857
Kiryl Kakareka <charleequa@gmail.com>
2021-08-24 14:46:21 +0300

    Pod deintegrate

--- Flickr/Flickr/ViewController.swift
+++ Flickr/Flickr/ViewController.swift
@@ -6,41 +6,36 @@
 //
 
 import UIKit
-import Alamofire
+import SafariServices
 
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemPink
         
         print("[1] Signing Requests")
         print(AuthService.shared.signature)
         print()
         
         print("[2] Request Token")
-        AF.request("https://www.flickr.com/services/oauth/request_token?oauth_callback=http://\(AuthService.shared.callback)&oauth_consumer_key=\(AuthService.shared.consumerKey)&oauth_nonce=\(AuthService.shared.nonce)&oauth_signature_method=\(AuthService.shared.signatureMethod)&oauth_timestamp=\(AuthService.shared.timestamp)&oauth_version=\(AuthService.shared.version)&oauth_signature=\(AuthService.shared.signature)").responseString { response in
-            switch response.result {
-            case .success(let data):
-                AuthService.shared.requestToken = data.components(separatedBy: "&oauth_token=").last?.components(separatedBy: "&oauth_token_secret").first
-                AuthService.shared.tokenSecret = data.components(separatedBy: "&oauth_token_secret=").last
-                print(AuthService.shared.tokenSecret!)
-            case .failure(let error):
-                print(error)
-            }
-        }
+//        AF.request("https://www.flickr.com/services/oauth/request_token?oauth_callback=http://\(AuthService.shared.callback)&oauth_consumer_key=\(AuthService.shared.consumerKey)&oauth_nonce=\(AuthService.shared.nonce)&oauth_signature_method=\(AuthService.shared.signatureMethod)&oauth_timestamp=\(AuthService.shared.timestamp)&oauth_version=\(AuthService.shared.version)&oauth_signature=\(AuthService.shared.signature)").responseString { response in
+//            switch response.result {
+//            case .success(let data):
+//                AuthService.shared.requestToken = data.components(separatedBy: "&oauth_token=").last?.components(separatedBy: "&oauth_token_secret").first
+//                AuthService.shared.tokenSecret = data.components(separatedBy: "&oauth_token_secret=").last
+//                print(AuthService.shared.tokenSecret!)
+//            case .failure(let error):
+//                print(error)
+//            }
+//        }
         print()
         
         print("[3] User Authorization")
-        AF.request("https://www.flickr.com/services/oauth/authorize?oauth_token=\(AuthService.shared.requestToken)").responseString { response in
-            switch response.result {
-            case .success(let data):
-                print(data)
-            case .failure(let error):
-                print(error)
-            }
+        if let url = URL(string: "https://www.flickr.com/services/oauth/authorize?oauth_token=\(String(describing: AuthService.shared.requestToken))") {
+            let vc = SFSafariViewController(url: url)
+            present(vc, animated: true)
         }
-        print()
     }
-    
 }
 
