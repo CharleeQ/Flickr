25aa9b556d67f6204d607571e59a48a8ab742ecd
Kiryl Kakareka <charleequa@gmail.com>
2021-08-25 17:26:34 +0300

    Handle the Callback

--- Flickr/Flickr/Services/AuthService.swift
+++ Flickr/Flickr/Services/AuthService.swift
@@ -6,30 +6,26 @@
 //
 
 import UIKit
-import SafariServices
+import AuthenticationServices
 
 class AuthService {
     static let shared = AuthService()
     
+    
     // MARK: - Parameters
+    
     private let consumerKey = "1877653cabad94b4cd42e56f49689e6c"
     private let consumerSecret = "f1938a9c14a1d472"
-    private let callback = "www.github.com"
-    private let nonce = "956r13465"
+    private let callback = "kiryl"
+    private let nonce = "956w13465"
     var signature: String {
         // Step zero: Signing requests
-        let string = "GET&https%3A%2F%2Fwww.flickr.com%2Fservices%2Foauth%2Frequest_token&oauth_callback%3Dhttp%253A%252F%252F\(callback)%26oauth_consumer_key%3D\(consumerKey)%26oauth_nonce%3D\(nonce)%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1305586162%26oauth_version%3D1.0"
+        let string = "GET&https%3A%2F%2Fwww.flickr.com%2Fservices%2Foauth%2Frequest_token&oauth_callback%3D\(callback)%253A%252F%252Fapp.com%26oauth_consumer_key%3D\(consumerKey)%26oauth_nonce%3D\(nonce)%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1305586162%26oauth_version%3D1.0"
         let encryptString = string.hmac(key: "\(consumerSecret)&")
         print("[SIGNATURE]: " + encryptString)
         
         return encryptString
     }
-    
-    
-    // MARK: - Tokens
-    
-    private var requestToken: String?
-    private var tokenSecret: String?
     
     
     // MARK: - Public functions
@@ -38,14 +34,7 @@ class AuthService {
         getARequestToken { result in
             switch result {
             case .success(let data):
-                self.getTheUserAuthorization(presenter: presenter, token: data) { result in
-                    switch result {
-                    case .success(_):
-                        self.exchangeTokens()
-                    case .failure(let error):
-                        print(error)
-                    }
-                }
+                self.getTheUserAuthorization(presenter: presenter, token: data)
             case .failure(let error):
                 print(error)
             }
@@ -53,7 +42,13 @@ class AuthService {
     }
     
     func handleURL(url: URL) {
-        
+        //kiryl://app.com?oauth_token=72157719725335319-78f57b08128fa85e&oauth_verifier=625c10dcf3cfcabd
+        print("Handle the Callback \(url.absoluteString)")
+        let verifier = url.absoluteString.components(separatedBy: "verifier=").last
+        let token = url.absoluteString.components(separatedBy: "&oauth_token=").last?.components(separatedBy: "&oauth_verifier").first
+        if let verifier = verifier, let token = token {
+            exchangeTokens(token: token, verifier: verifier)
+        }
     }
     
     
@@ -61,7 +56,7 @@ class AuthService {
     
     // First step
     private func getARequestToken(completion: @escaping (Result<String, Error>) -> Void) {
-        let url = URL(string: "https://www.flickr.com/services/oauth/request_token?oauth_callback=http://\(callback)&oauth_consumer_key=\(consumerKey)&oauth_nonce=\(nonce)&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1305586162&oauth_version=1.0&oauth_signature=\(signature)")
+        let url = URL(string: "https://www.flickr.com/services/oauth/request_token?oauth_callback=\(callback)://app.com&oauth_consumer_key=\(consumerKey)&oauth_nonce=\(nonce)&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1305586162&oauth_version=1.0&oauth_signature=\(signature)")
         if let url = url {
             let config = URLSessionConfiguration.default
             let session = URLSession(configuration: config)
@@ -70,9 +65,10 @@ class AuthService {
             session.dataTask(with: urlRequest) { data, _, error in
                 if let data = data {
                     let string = String(data: data, encoding: .utf8)
-                    AuthService.shared.requestToken = string?.components(separatedBy: "&oauth_token=").last?.components(separatedBy: "&oauth_token_secret").first
-                    AuthService.shared.tokenSecret = string?.components(separatedBy: "&oauth_token_secret=").last
-                    completion(.success(data.base64EncodedString()))
+                    let requestToken = string?.components(separatedBy: "&oauth_token=").last?.components(separatedBy: "&oauth_token_secret").first
+                    if let requestToken = requestToken {
+                        completion(.success(requestToken))
+                    }
                 } else if let error = error {
                     completion(.failure(error))
                 }
@@ -82,21 +78,31 @@ class AuthService {
     }
     
     // Second step
-    private func getTheUserAuthorization(presenter: UIViewController, token: String, completion: @escaping ((Result<String, Error>) -> Void)) {
-        if let requestToken = requestToken {
-            let url = URL(string: "https://www.flickr.com/services/oauth/authorize?oauth_token=\(requestToken)")
-            if let url = url {
-                let vc = SFSafariViewController(url: url)
-                DispatchQueue.main.async {
-                    presenter.present(vc, animated: true)
-                }
+    private func getTheUserAuthorization(presenter: UIViewController, token: String) {
+        let url = URL(string: "https://www.flickr.com/services/oauth/authorize?oauth_token=\(token)")
+        if let url = url {
+            let signSession = ASWebAuthenticationSession(url: url, callbackURLScheme: callback) { callbackURL, error in
+                guard error == nil, let callbackURL = callbackURL else { return }
+                
+                self.handleURL(url: callbackURL)
             }
+            signSession.presentationContextProvider = presenter as? ASWebAuthenticationPresentationContextProviding
+            signSession.start()
         }
     }
     
     // Third step
-    private func exchangeTokens() {
-        
+    private func exchangeTokens(token: String, verifier: String) {
+        let url = URL(string: "https://www.flickr.com/services/oauth/access_token?oauth_nonce=\(nonce)&oauth_timestamp=1305586309&oauth_verifier=\(verifier)&oauth_consumer_key=\(consumerKey)&oauth_signature_method=HMAC-SHA1&oauth_version=1.0&oauth_token=\(token)&oauth_signature=\(signature)")
+        if let url = url {
+            let config = URLSessionConfiguration.default
+            let session = URLSession(configuration: config)
+            
+            let urlRequest = URLRequest(url: url)
+            session.dataTask(with: urlRequest) { data, response, error in
+                print("SASI")
+            }
+        }
     }
 }
 
--- Flickr/Flickr/ViewController.swift
+++ Flickr/Flickr/ViewController.swift
@@ -6,12 +6,12 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
         
         AuthService.shared.login(presenter: self) { result in
             switch result {
@@ -24,3 +24,8 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: ASWebAuthenticationPresentationContextProviding {
+    func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
+        return view.window!
+    }
+}
